0f5635246cb79df4e0f40dd74f2876e1ecb81203
Steven Huasupoma <steven.huasupoma@coppel.com>
2024-07-01 17:51:09 -0600

    fundamentos de selenium parte 1

---
+++ src/test/java/uitest/m6/ConfiguringDriver.java
@@ -0,0 +1,63 @@
+package uitest.m6;
+
+import helper.DemoHelper;
+import helper.DriverFactory;
+import helper.Pages;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.CapabilityType;
+import org.testng.annotations.Test;
+
+import java.util.Map;
+
+public class ConfiguringDriver {
+    @Test
+    public void practiceWindowSize(){
+        WebDriver driver = DriverFactory.newDriver();
+        WebDriver.Window window = driver.manage().window();
+        window.maximize();
+        window.minimize();
+        window.setSize(new Dimension(500, 500));
+        window.setPosition(new Point(200, 200));
+        driver.get(Pages.INDEX);
+        DemoHelper.pause();
+        driver.quit();
+    }
+
+    @Test
+    public void practiceHeadlessMode(){
+        FirefoxOptions options = new FirefoxOptions();
+        options.addArguments("--headless");
+        WebDriver driver = new FirefoxDriver(options);
+        driver.get(Pages.INDEX);
+        DemoHelper.pause();
+        WebElement button = driver.findElement(By.id("register"));
+        System.out.println(button.getText());
+        driver.quit();
+    }
+
+    @Test
+    public void handleAlertsByDefault(){
+        FirefoxOptions options = new FirefoxOptions();
+        options.setUnhandledPromptBehaviour(UnexpectedAlertBehaviour.DISMISS);
+        //options.setCapability(CapabilityType.UNHANDLED_PROMPT_BEHAVIOUR, UnexpectedAlertBehaviour.DISMISS);
+        WebDriver driver = new FirefoxDriver(options);
+        driver.get(Pages.INDEX);
+        driver.findElement(By.id("clear")).click();
+        driver.findElement(By.id("register")).click();
+        DemoHelper.pause();
+    }
+
+    /* Esto es una prueba de selenium para emular un mobile
+    @Test
+    public void deviceEmulation(){
+        Map<String, String> mobileEmulation= Map.of("deviceName", "iPhone X");
+        FirefoxOptions options = new FirefoxOptions();
+        options.addArguments("mobileEmulation="+mobileEmulation);
+        WebDriver driver = new FirefoxDriver(options);
+        driver.get(Pages.INDEX);
+        DemoHelper.pause();
+    }*/
+}
