e9848af3d916f57f98001ba93e770d0ac51cd310
Steven Huasupoma <steven.huasupoma@coppel.com>
2024-07-01 21:26:23 -0600

    agregando practica logs de consola

--- src/test/java/uitest/m6/ConfiguringDriver.java
+++ src/test/java/uitest/m6/ConfiguringDriver.java
@@ -5,12 +5,20 @@ import helper.DriverFactory;
 import helper.Pages;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.logging.LogEntries;
+import org.openqa.selenium.logging.LogEntry;
+import org.openqa.selenium.logging.LogType;
+import org.openqa.selenium.logging.LoggingPreferences;
 import org.openqa.selenium.remote.CapabilityType;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.util.Map;
+import java.util.logging.Level;
 
 public class ConfiguringDriver {
     @Test
@@ -60,4 +68,31 @@ public class ConfiguringDriver {
         driver.get(Pages.INDEX);
         DemoHelper.pause();
     }*/
+
+    @Test
+    public void practiceConsoleLogs(){
+        //Se instancia el objeto LoginPreferences
+        LoggingPreferences logs = new LoggingPreferences();
+        logs.enable(LogType.BROWSER, Level.ALL);
+
+        //Instanciar el driver
+        EdgeOptions options = new EdgeOptions();
+        options.setCapability(EdgeOptions.LOGGING_PREFS,logs);
+        EdgeDriver driver =new  EdgeDriver(options);
+        driver.get(Pages.INDEX);
+        driver.findElement(By.id("register")).click();
+
+        //Mostrar los logs
+        LogEntries browserLogs = driver.manage().logs().get(LogType.BROWSER);
+        Assert.assertFalse(browserLogs.getAll().isEmpty());
+        browserLogs.forEach(System.out::println); //Este imprime la marca del tiempo del log
+        browserLogs.forEach(logEntry -> System.out.println(logEntry.getLevel() + "" + logEntry.getMessage()));
+
+        //Fallara la prueba, si encuentra error de estatus SEVERO
+        browserLogs.forEach(logEntry -> checkNoError(logEntry));
+    }
+
+    private void checkNoError(LogEntry logEntry) {
+        Assert.assertNotEquals(logEntry.getLevel().getName(),"SEVERE");
+    }
 }
